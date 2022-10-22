fec3a812cb2cf1390570a914d3b5078b9dd0a05a
SHAHBAZ HUSSAIN <90305433+shahbazhussaincse@users.noreply.github.com>
2022-10-22 09:43:40 +0530

    Java Programming
    Create a class Date with the variables to represent the day, month and year, use constructor to initialize the variables (use default date 01/01/1800), mutator and accessor methods, method to return the difference between two Date in days, method to add or subtract n number of days, toString method to return object as a string, equals method to check two Date objects are equal or not. Also validate the date in constructor and mutator methods. [Hint: Use private helper methods]

---
+++ Date.java
@@ -0,0 +1,18 @@
+package javatime;
+import java.time.*;
+import java.util.*;
+
+public class Date {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter year [YYYY] : ");
+        int y=sc.nextInt();
+        System.out.println("Enter month [MM] : ");
+        int m=sc.nextInt();
+        System.out.println("Enter day [DD] : ");
+        int d=sc.nextInt();
+
+        public int getYear()
+
+    }
+}
